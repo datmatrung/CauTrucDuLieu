97dadbccb8169eee57ab06d485aef42b0e2f15a4
datmatrung <46807674+datmatrung@users.noreply.github.com>
2019-12-26 02:32:52 +0700

    Add files via upload

--- TimKiem.cs
+++ TimKiem.cs
@@ -1,7 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
         static int[] NhapMang(int n)
         {
             int[] A;
-            A = new int[n + 1];
+            A = new int[n];
             for (int i = 0; i < n; i++)
             {
                 Console.Write($"Nhap A[{i}]: ");
@@ -21,10 +31,13 @@
         }
         static int TimKiemTuanTuLinhCanh(int[] A, int n, int x)
         {
-            A[n] = x;
+            int[] B = new int[n + 1];
+            Array.Copy(A, B, n);
+            B[n] = x;
             int i = 0;
-            while (x != A[i])
+            while (x != B[i])
                 i++;
+            if (i == n) i = -1;
             return i;
         }
         static int TimKiemNhiPhan(int[] A, int n, int x)
@@ -40,6 +53,14 @@
             }
             return -1;
         }
+        static int TimKiemNhiPhanDeQuy(int[] A, int left, int right, int x)
+        {
+            if (left > right) return -1;
+            int mid = (left + right) / 2;
+            if (A[mid] == x) return mid;
+            if (x < A[mid]) return TimKiemNhiPhanDeQuy(A, left, mid - 1, x);
+            return TimKiemNhiPhanDeQuy(A, mid + 1, right, x);
+        }
         static void Main(string[] args)
         {
             int[] A;
@@ -48,9 +69,12 @@
             A = NhapMang(n);
             Console.Write("Nhap phan tu can tim: ");
             int x = int.Parse(Console.ReadLine());
-            int i = TimKiemTuanTuVetCan(A, n, x);
-            int i = TimKiemTuanTuLinhCanh(A, n, x);
-            int i = TimKiemNhiPhan(A, n, x);
+            //int i = TimKiemTuanTuVetCan(A, n, x);
+            //int i = TimKiemTuanTuLinhCanh(A, n, x);
+            //int i = TimKiemNhiPhan(A, n, x);
+            int i = TimKiemNhiPhanDeQuy(A, 0, n-1, x);
             Console.Write($"{x} vi tri {i}");
             Console.ReadLine();
         }
+    }
+}
