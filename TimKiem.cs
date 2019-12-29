6fe821e5bdde5c4acba80797c772e85f96e2db72
datmatrung <46807674+datmatrung@users.noreply.github.com>
2019-12-29 21:43:21 +0700

    Update TimKiem.cs

--- TimKiem.cs
+++ TimKiem.cs
@@ -1,14 +1,3 @@
-        static int[] NhapMang(int n)
-        {
-            int[] A;
-            A = new int[n];
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write($"Nhap A[{i}]: ");
-                A[i] = int.Parse(Console.ReadLine());
-            }
-            return A;
-        }
         static int TimKiemTuanTuVetCan(int[] a, int n, int x)
         {
             int pos = -1;
@@ -65,16 +54,18 @@
         }
         static void Main(string[] args)
         {
-            int[] A;
-            Console.Write("Nhap so phan tu: ");
             int n = int.Parse(Console.ReadLine());
-            A = NhapMang(n);
-            Console.Write("Nhap phan tu can tim: ");
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = int.Parse(Console.ReadLine());
             int x = int.Parse(Console.ReadLine());
+            
             int i = TimKiemTuanTuVetCan(A, n, x);
             //int i = TimKiemTuanTuLinhCanh(A, n, x);
             //int i = TimKiemNhiPhan(A, n, x);
             //int i = TimKiemNhiPhanDeQuy(A, 0, n-1, x);
-            Console.Write($"{x} vi tri {i}");
+            if (pos == -1)
+                Console.WriteLine($"Khong tim thay {x}");
+            Console.Write($"{x} o vi tri {i}");
             Console.ReadLine();
         }
